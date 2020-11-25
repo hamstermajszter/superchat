f728e3122fe4d53156a4e1a72c28e880e4521879
Balazs Viczan <hamstermajszter@gmail.com>
2020-11-25 23:30:35 +0100

    add config

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  // your config
+  apiKey: "AIzaSyCytXzG-R1TvG9Q1fKvACk7dJZ0keCoacg",
+  authDomain: "superchat-cdc12.firebaseapp.com",
+  databaseURL: "https://superchat-cdc12.firebaseio.com",
+  projectId: "superchat-cdc12",
+  storageBucket: "superchat-cdc12.appspot.com",
+  messagingSenderId: "364248332122",
+  appId: "1:364248332122:web:55adbe3c683b99be6fa563"
+
 })
 
 const auth = firebase.auth();
